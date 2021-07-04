1eaf1b6d2dea2af3d249cc11ad121a76760ef058
jlbuild <juliabuildbot@gmail.com>
2021-07-04 21:41:39 +0000

    libjulia_jll build 1.7.0+2

--- src/wrappers/i686-linux-gnu-cxx11-julia_version+1.7.0.jl
+++ src/wrappers/i686-linux-gnu-cxx11-julia_version+1.7.0.jl
@@ -2,10 +2,11 @@
 export libjulia
 
 using LibUnwind_jll
+using LibUV_jll
 JLLWrappers.@generate_wrapper_header("libjulia")
 JLLWrappers.@declare_library_product(libjulia, "libjulia.so.1")
 function __init__()
-    JLLWrappers.@generate_init_header(LibUnwind_jll)
+    JLLWrappers.@generate_init_header(LibUnwind_jll, LibUV_jll)
     JLLWrappers.@init_library_product(
         libjulia,
         "lib/libjulia.so",
--- src/wrappers/i686-w64-mingw32-cxx03-julia_version+1.7.0.jl
+++ src/wrappers/i686-w64-mingw32-cxx03-julia_version+1.7.0.jl
@@ -2,10 +2,11 @@
 export libjulia
 
 using LibUnwind_jll
+using LibUV_jll
 JLLWrappers.@generate_wrapper_header("libjulia")
 JLLWrappers.@declare_library_product(libjulia, "libjulia.dll")
 function __init__()
-    JLLWrappers.@generate_init_header(LibUnwind_jll)
+    JLLWrappers.@generate_init_header(LibUnwind_jll, LibUV_jll)
     JLLWrappers.@init_library_product(
         libjulia,
         "bin\\libjulia.dll",
